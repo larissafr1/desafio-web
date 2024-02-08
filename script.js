eee6f21eddf30923035e9b9f250ff4baa672274d
larissafr1 <larissafelipereis7@gmail.com>
2024-02-08 15:22:28 -0300

    alterações

--- script.js
+++
@@ -1,6 +0,0 @@
-const darkModeToggle = document.getElementById('darkModeToggle');
-const body = document.body;
-
-darkModeToggle.addEventListener('click', () => {
-    body.classList.toggle('dark-mode');
-});
