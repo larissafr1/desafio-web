be59616580f3855004d3a585e73eef06b0097ba3
larissafr1 <larissafelipereis7@gmail.com>
2024-02-15 16:20:53 -0300

    90%

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const toggle = document.getElementById('toggle');
 
     toggle.addEventListener('change', () => {
-        document.body.classList.toggle('dark-mode'); // Toggle dark mode for body
-        document.querySelector('nav').classList.toggle('dark-mode'); // Toggle dark mode for nav
+        document.body.classList.toggle('darkmode');
+        document.querySelector('.header_navigation').classList.toggle('darkmode');
+        document.querySelector('.section-news').classList.toggle('darkmode');
+        document.querySelector('.price').classList.toggle('darkmode');
+        document.querySelector('.section-gif').classList.toggle('darkmode');
+        document.querySelector('.recomendations').classList.toggle('darkmode');
+        document.querySelector('.recomendations button').classList.toggle('darkmode');
     });
 });
